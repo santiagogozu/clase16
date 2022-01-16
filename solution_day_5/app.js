a5ee4a741082e2ecd53b7698d6ef32c9fb4c4968
santiagogozu <santiagogo1993@gmail.com>
2022-01-16 13:50:12 -0500

    pureba 3

--- solution_day_5/app.js
+++ solution_day_5/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+// const express = require('express');
+// const app = express();
+// app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando');
+// });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/home.html');
+// });
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
